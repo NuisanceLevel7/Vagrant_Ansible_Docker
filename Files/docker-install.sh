b7e9985770c74089b67aa908afa380e2d3601bb0
vicengle <victor.engle@gmail.com>
2017-08-27 14:33:24 -0400

    Added docker-machine to the provisioning script.

--- Files/docker-install.sh
+++ Files/docker-install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+l!/bin/bash
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
@@ -8,4 +8,7 @@ systemctl status docker
 curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-$(uname -s)-$(uname -m)"
 chmod +x /usr/local/bin/docker-compose
 docker-compose -v
+curl -L "https://github.com/docker/machine/releases/download/v0.9.0/docker-machine-$(uname -s)-$(uname -m)" -o /
+/usr/local/bin/docker-machine
+chmod +x /usr/local/bin/docker-machine
 

3ad302d906be3737557ff0764182e4d152a86c82
vicengle <victor.engle@gmail.com>
2017-08-30 18:46:32 -0400

    Automatically build and run the app container.

--- Files/docker-install.sh
+++ Files/docker-install.sh
@@ -1,16 +1,26 @@
-l!/bin/bash
+#!/bin/bash
 
+# Add the Docker repo and install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get install -y docker-ce
 systemctl status docker
+
+# Install docker-compose
 curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-$(uname -s)-$(uname -m)"
 chmod +x /usr/local/bin/docker-compose
 docker-compose -v
+
+# Install docker-machine
 curl -L "https://github.com/docker/machine/releases/download/v0.9.0/docker-machine-$(uname -s)-$(uname -m)" -o /
 /usr/local/bin/docker-machine
 chmod +x /usr/local/bin/docker-machine
+
+# Build and run the container
 cd /App
 tar -xvf ./App.tar
 rm App.tar
+docker image build . -t cluster
+docker container run -d -p 8080:80 cluster
+
